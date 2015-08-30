ced34b34c43c45b8d6874dfcfdd95769b33a8e76
Kendall Buchanan <kendall@kendagriff.com>
2015-08-29 21:03:51 -0600

    safety unload on load

--- src/fixtures/adapters/jdbc.clj
+++ src/fixtures/adapters/jdbc.clj
@@ -8,21 +8,25 @@
 (defn- clear-table! [spec table]
   (delete! spec table []))
 
+(defn- unload* [spec data]
+  (try
+    (doseq [[table _] data]
+      (clear-table! spec table))
+    (catch Exception e
+      (println (.getMessage e)))))
+
 (defrecord JDBCAdapter []
   fixtures.protocols.Loadable
   (load! [adapter {:keys [spec]} data]
     (try
+      (unload* spec data) ;; in case the system failed to unload properly on shutdown
       (doseq [[table records] data]
         (load-table! spec table records))
       (catch Exception e
         (println (.getMessage e)))))
 
-  (unload! [adapter {:keys [spec]} data]
-    (try
-      (doseq [[table _] data]
-        (clear-table! spec table))
-      (catch Exception e
-        (println (.getMessage e))))))
+  (unload! [_ {:keys [spec]} data]
+    (unload* spec data)))
 
 (defn jdbc-adapter
   "Constructor function for a Postgres adapter"
